997df38f2c0a6d6defdfeb5a0cae532910edafe5
lamaals <149162340+lamaals@users.noreply.github.com>
2025-10-24 13:32:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple smooth scroll or any interaction later if needed
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
